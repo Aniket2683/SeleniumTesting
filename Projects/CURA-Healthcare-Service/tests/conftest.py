ed2087e2055bc268aec161b38ba937645a65c367
Aniket Jaiswal <jaiswalaniket302@gmail.com>
2024-04-14 18:02:05 +0530

    Project on CURA Healthcare

--- Projects/CURA-Healthcare-Service/tests/conftest.py
+++ Projects/CURA-Healthcare-Service/tests/conftest.py
@@ -1,8 +1,5 @@
 import pytest
-import time
 from selenium import webdriver
-from selenium.webdriver import ActionChains
-from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
